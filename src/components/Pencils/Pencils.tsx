6a6b5e193701c4164d5f53c9239a149b38a1b839
romanyanke <roman@yanke.ru>
2020-08-16 10:39:13 +0300

    use localStorage to cache pencil description

--- src/components/Pencils/Pencils.tsx
+++ src/components/Pencils/Pencils.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { Description } from './Pencils.interface'
 import Details from '../Details'
+import { restorePencilMessage, storePencilMessage } from './Pencils.utils'
 
 const Pencils = () => {
-  const [info, setInfo] = useState('a collection of pencils')
+  const [description, setDescription] = useState(restorePencilMessage())
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('https://romanyanke.github.io/pencilbox-static/description/')
-      const info: Description = await response.json()
-      setInfo(info.description)
+      const { description } = await response.json()
+      setDescription(description)
     }
     fetchData()
   }, [])
+
+  useEffect(() => storePencilMessage(description), [description])
 
   return (
     <Details
       label="Hobby"
       value={
         <span>
-          I have <a href="https://pencil.yanke.ru/">{info}</a>.
+          I have <a href="https://pencil.yanke.ru/">{description}</a>.
         </span>
       }
     />
---
+++ src/components/Pencils/Pencils.utils.ts
@@ -0,0 +1,5 @@
+const pencilKey = 'yanke.ru:pencils'
+const fallbackMessage = 'a collection of pencils'
+
+export const restorePencilMessage = () => localStorage.getItem(pencilKey) || fallbackMessage
+export const storePencilMessage = (message: string) => localStorage.setItem(pencilKey, message)
