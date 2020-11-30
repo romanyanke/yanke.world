a895fe676014da7e9716aaf0a21c66fbf29965a6
romanyanke <roman@yanke.ru>
2020-11-30 22:44:17 +0300

    increase link space

--- src/components/Content/Links/Links.tsx
+++ src/components/Content/Links/Links.tsx
@@ -4,11 +4,11 @@ import styles from './Links.module.css'
 const Links = () => {
   const handle = 'romanyanke'
   const list = [
-    { url: `https://github.com/${handle}`, icon: icons.github, title: 'GitHub' },
-    { url: `https://www.instagram.com/${handle}`, icon: icons.instagram, title: 'Instagram' },
-    { url: `https://www.linkedin.com/in/${handle}`, icon: icons.linkedin, title: 'LinkedIn' },
-    { url: `https://t.me/${handle}`, icon: icons.telegram, title: 'Telegram' },
     { url: `https://twitter.com/${handle}`, icon: icons.twitter, title: 'Twitter' },
+    { url: `https://www.instagram.com/${handle}`, icon: icons.instagram, title: 'Instagram' },
+    { url: `https://t.me/${handle}`, icon: icons.telegram, title: 'Telegram' },
+    { url: `https://github.com/${handle}`, icon: icons.github, title: 'GitHub' },
+    { url: `https://www.linkedin.com/in/${handle}`, icon: icons.linkedin, title: 'LinkedIn' },
   ]
 
   return (
