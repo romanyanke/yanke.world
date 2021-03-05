6137f2e69b3ccd918eac4f74f1e7457f1edc8dfe
romanyanke <roman@yanke.ru>
2021-03-05 13:21:03 +0300

    update content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -3,22 +3,20 @@ import Links from './Links'
 import Pencils from './Pencils'
 
 const Content = () => (
-  <article>
-    <dl>
-      <Details label="Handle" value={<Links />} />
-      <Details
-        label="Position"
-        value={
-          <>
-            Lead Frontend Engineer @<a href="https://simplinic.de/">simplinic</a>
-          </>
-        }
-      />
-      <Details label="Email" value={<a href="mailto:roman@yanke.ru">roman@yanke.ru</a>} />
-      <Details label="Residence" value="Saint Petersburg, Russia 🇷🇺" />
-      <Pencils />
-    </dl>
-  </article>
+  <dl>
+    <Details label="Handle" value={<Links />} />
+    <Details
+      label="Position"
+      value={
+        <>
+          Lead Frontend Engineer @<a href="https://simplinic.de/">simplinic</a>
+        </>
+      }
+    />
+    <Pencils />
+    <Details label="Residence" value="Saint Petersburg, Russia 🇷🇺" />
+    <Details label="Email" value={<a href="mailto:roman@yanke.ru">roman@yanke.ru</a>} />
+  </dl>
 )
 
 export default Content
