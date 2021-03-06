032c41c909770e65a6fe532ad76080c35006930c
romanyanke <roman@yanke.ru>
2021-03-06 12:05:06 +0300

    infinit sky

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import profilePicture from './assets/romanyanke.jpg'
 import profilePicture2 from './assets/romanyanke@2x.jpg'
 import profilePicture3 from './assets/romanyanke@3x.jpg'
@@ -7,7 +8,23 @@ import { useDarkMode } from './Sky/Sky.hooks'
 
 const Header = () => {
   const [darkMode, toggleDarkMode] = useDarkMode()
+  const daysPassed = useRef(0)
   const name = 'Roman Yanke'
+
+  useEffect(() => {
+    if (darkMode) {
+      daysPassed.current++
+      if (daysPassed.current === 7) {
+        alert('Whoa! A week passed!')
+      }
+      if (daysPassed.current === 30) {
+        alert('Whoa! A month passed!')
+      }
+      if (daysPassed.current === 365) {
+        alert('Whoa! A year passed! Please stop!')
+      }
+    }
+  }, [darkMode])
 
   return (
     <header className={styles.root} onClick={toggleDarkMode}>
