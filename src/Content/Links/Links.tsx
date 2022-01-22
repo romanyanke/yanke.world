747597a0ee67a47178ef66a55dd7124e77b60b5a
romanyanke <roman@yanke.ru>
2022-01-22 11:24:57 +0300

    refactoring

--- src/Content/Links/Links.tsx
+++ src/Content/Links/Links.tsx
@@ -1,27 +1,26 @@
+import { FunctionComponent } from 'react'
 import { icons } from './icons'
 import styles from './Links.module.css'
 
-const Links = () => {
-  const handle = 'romanyanke'
-  const format = (domain: string) => `https://${domain}/${handle}`
-  const list = [
-    { url: format('twitter.com'), icon: icons.twitter, title: 'Twitter' },
-    { url: format('instagram.com'), icon: icons.instagram, title: 'Instagram' },
-    { url: format('t.me'), icon: icons.telegram, title: 'Telegram' },
-    { url: format('github.com'), icon: icons.github, title: 'GitHub' },
-    { url: format('linkedin.com/in'), icon: icons.linkedin, title: 'LinkedIn' },
-  ]
+const handle = 'romanyanke'
+const format = (domain: string) => `https://${domain}/${handle}`
+const list: [string, string, FunctionComponent][] = [
+  ['Twitter', format('twitter.com'), icons.twitter],
+  ['Instagram', format('instagram.com'), icons.instagram],
+  ['Telegram', format('t.me'), icons.telegram],
+  ['GitHub', format('github.com'), icons.github],
+  ['LinkedIn', format('linkedin.com/in'), icons.linkedin],
+]
 
-  return (
-    <div className={styles.root}>
-      <span>{handle}</span>
-      {list.map(({ url, icon: Icon, title }) => (
-        <a href={url} key={url} className={styles.link} title={title}>
-          <Icon />
-        </a>
-      ))}
-    </div>
-  )
-}
+const Links = () => (
+  <div className={styles.root}>
+    <span>{handle}</span>
+    {list.map(([title, url, Icon]) => (
+      <a href={url} key={url} className={styles.link} title={title}>
+        <Icon />
+      </a>
+    ))}
+  </div>
+)
 
 export default Links
