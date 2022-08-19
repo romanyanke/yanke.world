aa9c806064771d9de202c683fff1149e70a2562b
romanyanke <roman@yanke.ru>
2022-08-19 14:33:55 +0300

    --wip-- [skip ci]

--- src/script/theme.ts
+++ src/script/theme.ts
@@ -10,8 +10,19 @@ const isDark = daynight().dark
 document.body.classList.add(
   isDark ? Theme.dark : Theme.light,
 )
+updateMeta()
 
 export const toggleTheme = () => {
   document.body.classList.toggle(Theme.light)
   document.body.classList.toggle(Theme.dark)
+  updateMeta()
+}
+
+function updateMeta() {
+  const color = getComputedStyle(
+    document.body,
+  ).getPropertyValue('--background')
+  document
+    .querySelector('meta[name="theme-color"]')
+    ?.setAttribute('content', color)
 }
