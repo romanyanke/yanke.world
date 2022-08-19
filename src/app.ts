e8603db2e10ef1aabe54d0697ec45693027c84da
romanyanke <roman@yanke.ru>
2022-08-19 14:31:09 +0300

    offline

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import './script/theme'
 import './script/switch-theme'
 import './script/pencil'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(
+    new URL('./offline.js', import.meta.url),
+    { type: 'module', scope: '/' },
+  )
+}
