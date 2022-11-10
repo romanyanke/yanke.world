81a7ea9aa83c02409e47c62fe5ddd623e2a74e5c
romanyanke <roman@yanke.world>
2022-11-10 15:39:39 +0300

    fix statistic endpoint

--- src/script/pencil.ts
+++ src/script/pencil.ts
@@ -1,7 +1,9 @@
 const pencils = document.getElementById('pencils')
 
 if (pencils) {
-  fetch('https://romanyanke.github.io/pencilbox/statistic/')
+  fetch(
+    'https://romanyanke.github.io/pencilbox/en/statistic/',
+  )
     .then(response => response.text())
     .then(statistic => (pencils.innerText = statistic))
 }
